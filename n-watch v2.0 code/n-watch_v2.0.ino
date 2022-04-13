65e0b427ff13d1e18d1b0d5ff945514f5db8fa76
Naidan Demberel <61674803+naidandemberel@users.noreply.github.com>
2022-04-13 19:29:59 +0200

    Update n-watch_v2.0.ino
    Added battery analogRead() function

--- n-watch v2.0 code/n-watch_v2.0.ino
+++ n-watch v2.0 code/n-watch_v2.0.ino
@@ -6,12 +6,10 @@ void setup()
   rtc_clk_slow_freq_set(RTC_SLOW_FREQ_8MD256);// setting rtc slow frequency(more accurate in sleep modes)
   //use arduino-esp32 version 2.0.0 (2.0.2 won't work)
 
-
 #ifdef DEBUG
   Serial.begin(115200);
   Serial.println("n-watch v2.0");
 #endif
-
   pinMode(leftPin, INPUT);
   pinMode(rightPin, INPUT);
   
@@ -19,7 +17,7 @@ void setup()
   
   initLCD();
   initBL();
-
+  
   delay(100);
 }
 
@@ -30,28 +28,29 @@ void sleep()
   tft.writecommand(0x10);
   delay(5);// Sleep
 
-  //gpio_hold_dis(GPIO_NUM_5);
-  //gpio_deep_sleep_hold_dis();
-
+  //gpio_deep_sleep_hold_en();
+ 
   if (timeseted == true)
   {
     esp_wifi_stop();
+    btStop();
     esp_bt_controller_disable();
     esp_bluedroid_disable();
   }
-
+  
   //adc_power_off();
-  //btStop();
   //WiFi.mode(WIFI_OFF);
-
+  
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_13, 1);
   Serial.println("Sleping");
   Serial.flush();
   esp_light_sleep_start();
+  //esp_deep_sleep_start();
 }
 
 void onWakeup()
 {
+  //gpio_deep_sleep_hold_dis();
   digitalWrite(backLight, HIGH);
   tft.writecommand(0x11);
   delay(120);
@@ -69,21 +68,21 @@ boolean wakeupCheck()
     return 0;
   }
 }
+
+void battery()
+{
+  batvalue = analogRead(batteryPin)*(6.6/4096);
+  percent = (batvalue-1.55)/0.0.95*100;
+}
+
 void loop()
 {
-  //uint32_t xd = rtc_clk_slow_freq_get_hz();
-  //bool xd = rtc_clk_8m_enabled();
-  //bool xd = rtc_clk_8md256_enabled();
-  //uint32_t xd = getCpuFrequencyMhz();
-  //uint32_t xd = getApbFrequency();
-  //uint32_t xd = getXtalFrequencyMhz();
-  
   if (wakeupCheck())
   {
     if (timeseted == true)
     {
       mainDisplay();
-      //Serial.println(xd);
+      Serial.println(batvalue);
     }
     else
     {
